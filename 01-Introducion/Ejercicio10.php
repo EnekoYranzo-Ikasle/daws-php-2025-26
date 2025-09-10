d6f1bd5a720ad47601ffdb64394f745bea6ff67f
EnekoYranzo-Ikasle <eneko.yranzo@ikasle.egibide.org>
2025-09-10 19:26:32 +0200

    Hacer ejercicios 9 y 10

---
+++ 01-Introducion/Ejercicio10.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>01-Ejercicio10</title>
+</head>
+
+<body>
+  <h2>Ejercicio 10:</h2>
+  <h3>Solución:</h4>
+    <?php
+    function concatenar($a = "Egun", $b)
+    {
+      return $a . $b;
+    }
+
+    $resultado = concatenar("", "on");
+    echo $resultado;
+    ?>
+</body>
+
+</html>
---
+++ 01-Introducion/Ejercicio9.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>01-Ejercicio9</title>
+</head>
+
+<body>
+  <h2>Ejercicio 9:</h2>
+  <h3>Solución:</h4>
+    <?php
+    function concatenar($a, $b)
+    {
+      return $a . $b;
+    }
+
+    $resultado = concatenar("Egun", " on");
+    echo $resultado;
+    ?>
+</body>
+
+</html>
